3b05e7144d3776ffa2be6f28c96d0a6334e51eca
abhishek.lashley <abhishek.lashley@gmail.com>
2022-09-04 11:39:23 +0530

    Coding done and test case passed

--- src/main/java/com/jap/collectiondemo/MobileStore.java
+++ src/main/java/com/jap/collectiondemo/MobileStore.java
@@ -1,8 +1,11 @@
 package com.jap.collectiondemo;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class MobileStore {
@@ -24,7 +27,33 @@ public class MobileStore {
     //handle all the exceptions
     public List<Mobile> readMobileData(String fileName)
     {
+        int countLines = 0;
 
+        try{
+
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = bufferedReader.readLine();
+
+            while ((line = bufferedReader.readLine()) != null){
+                String[] values = line.split(splitBy);
+                String brandName = values[0];
+                String modelName = values[1];
+                double cost = Double.parseDouble(values[2]);
+                String screenSize = values[3];
+                String batteryLife = values[4];
+                String storageSpace = values[5];
+                int cameraPixels = Integer.parseInt(values[6]);
+
+                allMobiles.add(new Mobile(brandName,modelName,cost,screenSize,batteryLife,storageSpace,cameraPixels));
+
+            }
+
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
         return allMobiles;
     }
 
@@ -33,7 +62,13 @@ public class MobileStore {
     public List<Mobile> findPhoneByBrand(String brandName)
     {
         List<Mobile> mobilesByBrand = new ArrayList<>();
-
+        Iterator<Mobile> iterator = allMobiles.iterator();
+        while(iterator.hasNext()){
+            Mobile obj = iterator.next();
+            if(obj.getBrandName().equals(brandName)){
+                mobilesByBrand.add(obj);
+            }
+        }
         return mobilesByBrand;
     }
 
@@ -42,7 +77,13 @@ public class MobileStore {
     public List<Mobile> findPhoneCostMoreThan$500()
     {
         List<Mobile> mobilesMoreThan500 = new ArrayList<>();
-
+        Iterator<Mobile> iterator = allMobiles.iterator();
+        while(iterator.hasNext()){
+            Mobile obj = iterator.next();
+            if (obj.getCost() > 500){
+                mobilesMoreThan500.add(obj);
+            }
+        }
         return mobilesMoreThan500;
     }
 
@@ -51,8 +92,30 @@ public class MobileStore {
     public List<Mobile> findPhonePixelMoreThan12MP()
     {
         List<Mobile> mobilesMoreThan12MP = new ArrayList<>();
-
+        Iterator<Mobile> iterator = allMobiles.iterator();
+        while(iterator.hasNext()){
+            Mobile obj = iterator.next();
+            if (obj.getCameraPixels() > 12){
+                mobilesMoreThan12MP.add(obj);
+            }
+        }
         return mobilesMoreThan12MP;
+    }
+
+    public static void main(String[] args) {
+        MobileStore mobileStore = new MobileStore();
+        String fileName = "mobile.csv";
+        System.out.println("Reading mobile data");
+        System.out.println(mobileStore.readMobileData(fileName));
+        System.out.println();
+        System.out.println("Finding phones by Brand");
+        System.out.println(mobileStore.findPhoneByBrand("Samsung"));
+        System.out.println();
+        System.out.println("Finding phones which are costlier than 500");
+        System.out.println(mobileStore.findPhoneCostMoreThan$500());
+        System.out.println();
+        System.out.println("Finding phones which have camera pixels more than 12 MP");
+        System.out.println(mobileStore.findPhonePixelMoreThan12MP());
     }
 
 
